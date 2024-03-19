ca097f3f84d4721c21e5ab8c5f6c8295712789b8
ucha <uchaucha419@gmail.com>
2024-03-19 16:23:20 +0400

    implemented basic authentication

---
+++ src/main/java/com/example/springSecurity/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.example.springSecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> {
+            ((AuthorizeHttpRequestsConfigurer.AuthorizedUrl) requests.anyRequest()).authenticated();
+        });
+        http.httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+}
