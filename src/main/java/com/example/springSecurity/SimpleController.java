d895902086533579e69b2fb46cf991974217f104
ucha <uchaucha419@gmail.com>
2024-03-19 14:40:38 +0400

    first commit

---
+++ src/main/java/com/example/springSecurity/SimpleController.java
@@ -0,0 +1,16 @@
+package com.example.springSecurity;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class SimpleController {
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> helloController() {
+        return ResponseEntity.ok("Hello World!");
+    }
+}
